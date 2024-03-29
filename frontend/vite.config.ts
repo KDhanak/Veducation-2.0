3740c4e02860d83a554de1b2341373a933e51aac
KDhanak <kishandhanak306@gmail.com>
2024-03-30 00:36:49 +1100

    Finally, working. Vite has to be configured to access different host, such as docker's bridging network.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // For docker to to access frontend.
+    port: 3000 // To host the frontend on port 3000 instead of default.
+  }
 })
