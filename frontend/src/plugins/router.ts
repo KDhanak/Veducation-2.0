beb7675b72fbec002e094ee97dfca1ff17ca039b
KDhanak <kishandhanak306@gmail.com>
2024-10-16 21:40:28 +1100

    router testing.

--- frontend/src/plugins/router.ts
+++ frontend/src/plugins/router.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
     {
-        path: "/home",
+        path: "/",
         name: "Home",
         component: () => import("../components/Home.vue"),
     },
