346d5f64cd7d392ac5c04dc53f3c2592b268eb7f
KDhanak <kishandhanak306@gmail.com>
2024-10-17 14:44:59 +1100

    400 bad request issue.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['veducation-backend.onrender.com']
+ALLOWED_HOSTS = ['veducation-2-0.onrender.com/']
 
 CSRF_TRUSTED_ORIGINS = ['https://veducation-backend.onrender.com']
 
