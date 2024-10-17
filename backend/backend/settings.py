ff1c007dbb9d4b4c9d0d55fa30c4635a25f6a860
KDhanak <kishandhanak306@gmail.com>
2024-10-17 16:11:15 +1100

    updated the cors-origins.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,19 +111,10 @@ SIMPLE_JWT = {
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
-CORS_ALLOWED_ORIGINS = [
-    "https://veducation-2-0.onrender.com",
-]
+CORS_ALLOWED_ORIGINS = CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
